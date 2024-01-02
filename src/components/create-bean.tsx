31064b4d9e8baa9322d9fc256fbf63df3fece636
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2024-01-02 11:18:06 -0700

    starting formschema and form validations for coffee beans

--- src/components/create-bean.tsx
+++ src/components/create-bean.tsx
@@ -1,38 +1,42 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 import { api } from "~/trpc/react";
 import { Button } from "./ui/button";
+import { Form } from "./ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const FormSchema = z.object({
+  name: z.string(),
+});
+
+type FormType = z.infer<typeof FormSchema>;
 
 export function CreateBean() {
   const router = useRouter();
-  const [name, setName] = useState("");
+  const form = useForm<FormType>();
 
   const createCoffeeBean = api.coffeeBean.create.useMutation({
     onSuccess: () => {
       router.refresh();
-      setName("");
+      form.reset();
     },
   });
 
+  const onSubmit = (data: FormType) => {
+    createCoffeeBean.mutate({ name: data.name });
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        createCoffeeBean.mutate({ name });
-      }}
-    >
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <Button type="submit" disabled={createCoffeeBean.isLoading}>
-        {createCoffeeBean.isLoading ? "Submitting..." : "Submit"}
-      </Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Name" />
+        <Button type="submit" disabled={createCoffeeBean.isLoading}>
+          {createCoffeeBean.isLoading ? "Submitting..." : "Submit"}
+        </Button>
+      </form>
+    </Form>
   );
 }
